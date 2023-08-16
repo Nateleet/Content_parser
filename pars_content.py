43500fe7ff051eb1ede55eacacd395578947ebd7
Nateleet <122318291+Nateleet@users.noreply.github.com>
2023-08-16 20:04:06 +0300

    Update pars_content.py

--- pars_content.py
+++ pars_content.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-url = "" # Your site, example - https://www.nytimes.com/
+url = "https://www.nytimes.com/" # Your site, example - https://www.nytimes.com/
 
 
 
@@ -13,11 +13,13 @@ def pars(url):
     x = ("".join(part))
     r = requests.get(url)
     sp = bs(r.text, "html.parser")
-    with open("data1.txt", "w", encoding="utf-8") as f:
+    with open("data2.txt", "w", encoding="utf-8") as f:
         for tag in sp.find_all("a"):
             url = tag.get("href")
             if url is None:
                 continue
+            if url[0] == "/":
+                f.write("\n" + x + url)
             if "html" in url:
                 f.write("\n" + url)
 
